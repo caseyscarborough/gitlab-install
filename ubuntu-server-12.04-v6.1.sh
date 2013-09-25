646b2f50eec27440660af57dac33c692c43a1249
Casey Scarborough <caseyscarborough@gmail.com>
2013-09-24 21:01:16 -0400

    Update usage info for Ubuntu 12.04 install script.

--- ubuntu-server-12.04-v6.1.sh
+++ ubuntu-server-12.04-v6.1.sh
@@ -9,7 +9,8 @@
 # including all dependencies.
 #
 # USAGE
-# DOMAIN_VAR=gitlab.example.com sudo bash gitlab.sh
+# curl https://raw.github.com/caseyscarborough/gitlab-install/master/ubuntu-server-12.04-v6.1.sh | 
+#   DOMAIN_VAR=gitlab.example.com sudo bash
 
 APP_ROOT="/home/git/gitlab"
 APP_USER="git"
